a212751c3a5c0c0c8e327350832d956e79c85c5c
Victor Turra <victor.florencio@univem.edu.br>
2022-01-20 14:38:57 -0300

    feat: Implement authentication endpoint

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle an authentication attempt.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::guard()->attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return response()->json([], 204);
+        }
+
+        return response()->json(['error' => 'Email e/ou senha incorretos'], 400);
+    }
+}
